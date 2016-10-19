66c2e3dc2336e2bee076ced0cf6afd887dd448ab
leeyimin <imleeyimin@gmail.com>
2016-10-19 21:19:35 +0800

    Add storage to LogicManager and Command

--- src/main/java/seedu/taskman/logic/commands/Command.java
+++ src/main/java/seedu/taskman/logic/commands/Command.java
@@ -4,12 +4,14 @@ import seedu.taskman.commons.core.EventsCenter;
 import seedu.taskman.commons.core.Messages;
 import seedu.taskman.commons.events.ui.IncorrectCommandAttemptedEvent;
 import seedu.taskman.model.Model;
+import seedu.taskman.storage.Storage;
 
 /**
  * Represents a command with hidden internal logic and the ability to be executed.
  */
 public abstract class Command {
     protected Model model;
+    protected Storage storage;
 
     /**
      * Constructs a feedback message to summarise an operation that displayed a listing of tasks.
@@ -35,6 +37,13 @@ public abstract class Command {
      */
     public void setData(Model model) {
         this.model = model;
+    }
+
+    /**
+     * Set the storage object required for any execution of a command
+     */
+    public void setStorage(Storage storage){
+        this.storage = storage;
     }
 
     /**
