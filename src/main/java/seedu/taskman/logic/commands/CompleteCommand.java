71a76929ffb3bcd13eccbc3bcbffe51067856e99
Inaba Kazuhiro <inaba.k94@gmail.com>
2016-10-20 13:46:19 +0800

    Minor edit based on peer review

--- src/main/java/seedu/taskman/logic/commands/CompleteCommand.java
+++ src/main/java/seedu/taskman/logic/commands/CompleteCommand.java
@@ -15,42 +15,42 @@ import seedu.taskman.model.event.UniqueActivityList;
  * 		 and EditCommand (make a new Activity with the correct status and then deleting the old one)
  */
 public class CompleteCommand extends Command {
-	
-	public static final String COMMAND_WORD = "complete";
-	private static final String STATUS_COMPLETE = "complete";
-	
-	public static final String MESSAGE_USAGE = COMMAND_WORD + ": Marks an existing task as complete./n"
+
+    public static final String COMMAND_WORD = "complete";
+    private static final String STATUS_COMPLETE = "complete";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Marks an existing task as complete./n"
             + "Parameters: INDEX\n"
             + "Example: " + COMMAND_WORD
             + " 1";
 
     public static final String MESSAGE_SUCCESS = "Task completed: %1$s";
     public static final String MESSAGE_DUPLICATE_TASK = "A task with the same name already exists in TaskMan";
-    
+
     private Activity.ActivityType activityType;
     private Activity activityToComplete;
     private Activity afterComplete;
     private int targetIndex;
 
     public CompleteCommand(int targetIndex) {
-    	this.targetIndex = targetIndex;
-	}
+        this.targetIndex = targetIndex;
+    }
 
-	@Override
-	public CommandResult execute() {
-		assert model != null;
-		
-		try {
+    @Override
+    public CommandResult execute() {
+        assert model != null;
+
+        try {
             initMembers();
         } catch (IllegalValueException e) {
             return new CommandResult(e.getMessage());
         }
-        		
+
         try {
             model.deleteActivity(activityToComplete);
             model.addActivity(afterComplete);
             return new CommandResult(String.format(MESSAGE_SUCCESS, afterComplete.getTitle().title));
-        } catch (UniqueActivityList.ActivityNotFoundException pnfe) {
+        } catch (UniqueActivityList.ActivityNotFoundException notFound) {
             indicateAttemptToExecuteIncorrectCommand();
             return new CommandResult(Messages.MESSAGE_INVALID_EVENT_DISPLAYED_INDEX);
         } catch (UniqueActivityList.DuplicateActivityException e) {
@@ -61,10 +61,10 @@ public class CompleteCommand extends Command {
             }
             throw new AssertionError("Activities with duplicate titles exists in data!", null);
         }
-	}
+    }
 
-	private void initMembers() throws IllegalValueException {
-	    UnmodifiableObservableList<Activity> lastShownList = model.getFilteredActivityList();
+    private void initMembers() throws IllegalValueException {
+        UnmodifiableObservableList<Activity> lastShownList = model.getFilteredActivityList();
 
         if (lastShownList.size() < targetIndex) {
             indicateAttemptToExecuteIncorrectCommand();
@@ -73,29 +73,29 @@ public class CompleteCommand extends Command {
 
         activityToComplete = lastShownList.get(targetIndex - 1);
         activityType = activityToComplete.getType();
-        
+
         switch (activityType){
-            case EVENT: {
-                throw new IllegalValueException(Messages.MESSAGE_INVALID_COMMAND_FOR_EVENT);
-            }
-            case TASK: {
-                Task task = new Task(
-                        activityToComplete.getTitle(),
-                        activityToComplete.getTags(),
-                        activityToComplete.getDeadline().orElse(null),
-                        activityToComplete.getSchedule().orElse(null),
-                        activityToComplete.getFrequency().orElse(null));
-                try {
-                    task.setStatus(new Status(STATUS_COMPLETE));
-                } catch (IllegalValueException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-                afterComplete = new Activity(task);
-            }
-            default: {
-                assert false : "Activity is neither an event nor a task.";
-            }
+        case EVENT: {
+            throw new IllegalValueException(Messages.MESSAGE_INVALID_COMMAND_FOR_EVENT);
         }
-	}
+        case TASK: {
+            Task task = new Task(
+                    activityToComplete.getTitle(),
+                    activityToComplete.getTags(),
+                    activityToComplete.getDeadline().orElse(null),
+                    activityToComplete.getSchedule().orElse(null),
+                    activityToComplete.getFrequency().orElse(null));
+            try {
+                task.setStatus(new Status(STATUS_COMPLETE));
+            } catch (IllegalValueException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            afterComplete = new Activity(task);
+        }
+        default: {
+            assert false : "Activity is neither an event nor a task.";
+        }
+        }
+    }
 }
