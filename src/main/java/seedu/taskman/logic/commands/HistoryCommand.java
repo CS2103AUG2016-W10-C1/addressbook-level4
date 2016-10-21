af99f1ddb7d6e3b45597425451d203e243b304d6
Jia Yee <jiayeerawr@gmail.com>
2016-10-21 22:28:41 +0800

    Display more indicative texts for command history

--- src/main/java/seedu/taskman/logic/commands/HistoryCommand.java
+++ src/main/java/seedu/taskman/logic/commands/HistoryCommand.java
@@ -8,17 +8,25 @@ public class HistoryCommand extends Command {
     		+ Command.CAPACITY_HISTORY_COMMAND
     		+ " most recently executed commands.\n"
             + "Example: " + COMMAND_WORD;
+    
+    private static final int HISTORY_BULLET_FIRST = 1;
+    private static final String HISTORY_EMPTY_PLACEHOLDER = "\tNIL";
 
     public HistoryCommand() {}
 	
 	@Override
 	public CommandResult execute() {
 		super.getInputHistory().pop(); // pop history itself
-		StringBuilder builder = new StringBuilder();
+		StringBuilder builder = new StringBuilder("Command History:\n");
+		int commandCount = HISTORY_BULLET_FIRST;
 		for (String command : super.getInputHistory()) { // most recently executed command is at the head
+		    builder.append(String.format("\t%d. ", commandCount++));
 			builder.append(command);
 			builder.append("\n");
 		}
+		if (commandCount == HISTORY_BULLET_FIRST) {
+		    builder.append(HISTORY_EMPTY_PLACEHOLDER);
+		} 
 		return new CommandResult(builder.toString().trim());
 	}
 
