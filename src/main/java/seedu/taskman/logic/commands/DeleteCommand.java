71a298fe8654f6e2e97b9280f071d7b9a31721b5
Jia Yee <jiayeerawr@gmail.com>
2016-10-23 01:42:31 +0800

    Parse and act on "delete all"
    1. Parsing needs to be more sophisticated (I don't understand the parsing...)
    2. For unknown reasons, it will get buggy if the whole filtered list is dumped into deleteActivities

--- src/main/java/seedu/taskman/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/taskman/logic/commands/DeleteCommand.java
@@ -5,6 +5,9 @@ import seedu.taskman.commons.core.UnmodifiableObservableList;
 import seedu.taskman.model.event.Activity;
 import seedu.taskman.model.event.UniqueActivityList.ActivityNotFoundException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Deletes a task identified using it's last displayed index from the task man.
  */
@@ -18,6 +21,8 @@ public class DeleteCommand extends Command {
             + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_DELETE_EVENT_SUCCESS = "Deleted Task: %1$s";
+    public static final String MESSAGE_DELETE_ALL_EVENT_SUCCESS = "Deleted Task(s):";
+    public static final String MESSAGE_DELETE_ALL_EVENT_SUCCESS_SEPARATOR = "\n\t";
 
     public final int targetIndex;
 
@@ -31,6 +36,24 @@ public class DeleteCommand extends Command {
 
         UnmodifiableObservableList<Activity> lastShownList = model.getFilteredActivityList();
 
+        if (targetIndex == 0) {
+            StringBuilder builder = new StringBuilder(MESSAGE_DELETE_ALL_EVENT_SUCCESS);
+            try {
+                List<Activity> activityList = new ArrayList<>();
+                for (int i = 0; i < lastShownList.size(); i++) {
+                    Activity activityToDelete = lastShownList.get(i);
+                    activityList.add(activityToDelete);
+                    builder.append(MESSAGE_DELETE_ALL_EVENT_SUCCESS_SEPARATOR);
+                    builder.append(activityToDelete.getTitle().title);
+                }
+                model.deleteActivities(activityList);
+            } catch (ActivityNotFoundException pnfe) {
+                assert false: "The target task cannot be missing";
+            }
+
+            return new CommandResult(builder.toString());
+        }
+
         if (lastShownList.size() < targetIndex) {
             indicateAttemptToExecuteIncorrectCommand();
             return new CommandResult(Messages.MESSAGE_INVALID_EVENT_DISPLAYED_INDEX);
@@ -41,7 +64,7 @@ public class DeleteCommand extends Command {
         try {
             model.deleteActivity(activityToDelete);
         } catch (ActivityNotFoundException pnfe) {
-            assert false : "The target task cannot be missing";
+            assert false: "The target task cannot be missing";
         }
 
         return new CommandResult(String.format(MESSAGE_DELETE_EVENT_SUCCESS, activityToDelete));
