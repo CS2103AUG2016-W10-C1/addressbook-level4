9422a31246922425ea753a6eadef4219498c3ee7
leeyimin <imleeyimin@gmail.com>
2016-10-26 00:28:55 +0800

    enable helptest

--- src/test/java/guitests/HelpWindowTest.java
+++ src/test/java/guitests/HelpWindowTest.java
@@ -1,13 +1,11 @@
 package guitests;
 
 import guitests.guihandles.HelpWindowHandle;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-// todo: should fix
-@Ignore
+
 public class HelpWindowTest extends TaskManGuiTest {
 
     @Test
