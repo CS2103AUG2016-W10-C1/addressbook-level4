a4696b3d5fe876d3907cd43f9876f08b8f5d2250
leeyimin <imleeyimin@gmail.com>
2016-10-27 13:06:59 +0800

    set wrap text property for result display

--- src/main/java/seedu/taskman/ui/ResultDisplay.java
+++ src/main/java/seedu/taskman/ui/ResultDisplay.java
@@ -37,6 +37,8 @@ public class ResultDisplay extends UiPart {
         resultDisplayArea.getStyleClass().add(STATUS_BAR_STYLE_SHEET);
         resultDisplayArea.setText("");
         resultDisplayArea.textProperty().bind(displayed);
+        resultDisplayArea.setWrapText(true);
+        resultDisplayArea.setFocusTraversable(false);
         FxViewUtil.applyAnchorBoundaryParameters(resultDisplayArea, 0.0, 0.0, 0.0, 0.0);
         mainPane.getChildren().add(resultDisplayArea);
         FxViewUtil.applyAnchorBoundaryParameters(mainPane, 0.0, 0.0, 0.0, 0.0);
